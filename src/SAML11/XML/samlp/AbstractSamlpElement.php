ed4d79939b8827501c2643fa8fca3b26e1416876
Tim van Dijen <tvdijen@gmail.com>
2024-07-17 22:39:58 +0200

    Add samlp:RespondWith

---
+++ src/SAML11/XML/samlp/AbstractSamlpElement.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\SAML11\XML\samlp;
+
+use SimpleSAML\SAML11\Constants as C;
+use SimpleSAML\XML\AbstractElement;
+
+/**
+ * Abstract class to be implemented by all the classes in this namespace
+ *
+ * @package simplesamlphp/saml11
+ */
+abstract class AbstractSamlpElement extends AbstractElement
+{
+    /** @var string */
+    public const NS = C::NS_SAMLP;
+
+    /** @var string */
+    public const NS_PREFIX = 'samlp';
+}
