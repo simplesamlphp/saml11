065aa27ab88c10e0652d9f18f4244fc619808a4c
Tim van Dijen <tvdijen@gmail.com>
2024-07-31 21:31:11 +0200

    Add samlp:StatusDetail

---
+++ tests/src/SAML11/XML/samlp/StatusDetailTest.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\SAML11\XML\saml;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\SAML11\Constants as C;
+use SimpleSAML\SAML11\XML\samlp\AbstractSamlpElement;
+use SimpleSAML\SAML11\XML\samlp\AbstractStatusDetailType;
+use SimpleSAML\SAML11\XML\samlp\StatusDetail;
+use SimpleSAML\XML\Chunk;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\TestUtils\SchemaValidationTestTrait;
+use SimpleSAML\XML\TestUtils\SerializableElementTestTrait;
+
+use function dirname;
+use function strval;
+
+/**
+ * Class \SimpleSAML\SAML11\XML\samlp\StatusDetailTest
+ *
+ * @package simplesamlphp/saml11
+ */
+#[CoversClass(StatusDetail::class)]
+#[CoversClass(AbstractStatusDetailType::class)]
+#[CoversClass(AbstractSamlpElement::class)]
+final class StatusDetailTest extends TestCase
+{
+    use SchemaValidationTestTrait;
+    use SerializableElementTestTrait;
+
+    /**
+     */
+    public static function setUpBeforeClass(): void
+    {
+        self::$schemaFile = dirname(__FILE__, 6) . '/resources/schemas/oasis-sstc-saml-schema-protocol-1.1.xsd';
+
+        self::$testedClass = StatusDetail::class;
+
+        self::$xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 5) . '/resources/xml/samlp_StatusDetail.xml',
+        );
+    }
+
+
+    /**
+     */
+    public function testMarshalling(): void
+    {
+        $document = DOMDocumentFactory::fromString(
+            '<ssp:Cause xmlns:ssp="urn:custom:ssp">org.sourceid.websso.profiles.idp.FailedAuthnSsoException</ssp:Cause>',
+        );
+
+        $statusDetail = new StatusDetail([new Chunk($document->documentElement)]);
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($statusDetail),
+        );
+    }
+
+
+    /**
+     * Adding an empty StatusDetail element should yield an empty element.
+     */
+    public function testMarshallingEmptyElement(): void
+    {
+        $samlpns = C::NS_SAMLP;
+        $statusDetail = new StatusDetail([]);
+        $this->assertEquals(
+            "<samlp:StatusDetail xmlns:samlp=\"$samlpns\"/>",
+            strval($statusDetail),
+        );
+        $this->assertTrue($statusDetail->isEmptyElement());
+    }
+}
