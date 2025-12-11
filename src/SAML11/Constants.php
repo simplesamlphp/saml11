6010a63e4f9dad28241d806598a2f71c82fadcb6
Tim van Dijen <tvdijen@gmail.com>
2025-12-11 10:26:51 +0100

    Add missing constant

--- src/SAML11/Constants.php
+++ src/SAML11/Constants.php
@@ -67,6 +67,11 @@ class Constants extends \SimpleSAML\XMLSecurity\Constants
     public const AC_UNSPECIFIED = 'urn:oasis:names:tc:SAML:1.0:am:unspecified';
 
     /**
+     * Artifact subject confirmation method.
+     */
+    public const CM_ARTIFACT = 'urn:oasis:names:tc:SAML:1.0:cm:artifact';
+
+    /**
      * Bearer subject confirmation method.
      */
     public const CM_BEARER = 'urn:oasis:names:tc:SAML:1.0:cm:bearer';
